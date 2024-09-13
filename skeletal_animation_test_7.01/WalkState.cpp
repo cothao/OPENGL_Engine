5f19be42476f938e206b75eecee8738056aba8df
Collin Thao <collinthao685@gmail.com>
2024-09-12 23:55:57 -0500

    New update on collissions

--- skeletal_animation_test_7.01/WalkState.cpp
+++ skeletal_animation_test_7.01/WalkState.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18cf3b90b816a49e1498b30fcf9b6303ea9eb5ecaf242627ce763a1ae7eb2e75
-size 2513
+oid sha256:a4ee02fc041455179cafe4b68f5f8fbcd135d87b9a68f0516a9c6af4ee17d48e
+size 258
